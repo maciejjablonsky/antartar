3aa41f0226da7e3cba83498dadb37ea6e0f0c868
maciejjablonsky <maciekjablonsky@gmail.com>
2022-09-18 23:43:16 +0200

    Added checking validation layers - doesn't work yet

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout, CMake
+from conans.tools import environment_append
 
 class AntartarConanFile(ConanFile):
     name = "antartar"
@@ -8,6 +9,9 @@ class AntartarConanFile(ConanFile):
     
     def generate(self):
         tc = CMakeToolchain(self)
+    
+        tc.preprocessor_definitions["ANTARTAR_IS_DEBUG"] = 1 if self.settings.build_type == "Debug" else 0
+        tc.preprocessor_definitions["ANTARTAR_IS_RELEASE"] = 1 if self.settings.build_type == "Release" else 0
         tc.generate()
 
         deps = CMakeDeps(self)
@@ -21,16 +25,25 @@ class AntartarConanFile(ConanFile):
         self.build_requires("cmake/[>=3.24]")
 
     def requirements(self):
-        self.requires("vulkan-loader/1.3.224.0")
+        self.requires("vulkan-loader/1.3.216.0")
         self.requires("fmt/9.1.0")
         self.requires("glfw/3.3.8")
         self.requires("ms-gsl/4.0.0")
         self.requires("range-v3/0.12.0")
+        self.requires("vulkan-validationlayers/1.3.216.0")
+
+    def _vulkan_layers(self):
+        layers = {}
+        if self.settings.build_type == "Debug":
+            layers["VK_INSTANCE_LAYERS"] = "VK_LAYER_LUNARG_api_dump;VK_LAYER_KHRONOS_validation"
+        return layers
+
 
     def build(self):
-        cmake = CMake(self)
-        if self.should_configure:
-            cmake.configure()
-        if self.should_build:
-            cmake.build()
+        with environment_append(self._vulkan_layers()):
+            cmake = CMake(self)
+            if self.should_configure:
+                cmake.configure()
+            if self.should_build:
+                cmake.build()
     
--- include/antartar/vk.hpp
+++ include/antartar/vk.hpp
@@ -7,28 +7,32 @@
 
 namespace antartar
 {
+	const std::array<std::string_view, 1> validation_layers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+
+	constexpr bool enable_validation_layers = ANTARTAR_IS_DEBUG;
+
+
 	class vk {
 	private:
 		VkInstance instance_;
 
-		inline void initialize_()
+		inline bool check_validation_layer_support()
 		{
-			VkApplicationInfo app_info{};
-			app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-			app_info.pApplicationName = "antartar";
-			app_info.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
-			app_info.pEngineName = "no engine";
-			app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-			app_info.apiVersion = VK_API_VERSION_1_0;
+			uint32_t layer_count = 0;
+			vkEnumerateInstanceLayerProperties(std::addressof(layer_count), nullptr);
 
-			VkInstanceCreateInfo create_info{};
-			create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-			create_info.pApplicationInfo = std::addressof(app_info);
-			uint32_t glfw_extensions_count = 0;
-			create_info.ppEnabledExtensionNames = glfwGetRequiredInstanceExtensions(std::addressof(glfw_extensions_count));
-			create_info.enabledExtensionCount = glfw_extensions_count;
-			create_info.enabledLayerCount = 0;
+			std::pmr::vector<VkLayerProperties> available_layers(layer_count);
+			vkEnumerateInstanceLayerProperties(std::addressof(layer_count), available_layers.data());
+			return  ranges::all_of(validation_layers, [&available_layers](std::string_view layer_name) {
+				return ranges::contains(available_layers, layer_name, [](const VkLayerProperties& prop) {
+					return prop.layerName; });
+				});
+		}
 
+		inline	void log_available_extensions() const
+		{
 			uint32_t extension_count = 0;
 			vkEnumerateInstanceExtensionProperties(nullptr, std::addressof(extension_count), nullptr);
 			std::pmr::vector<VkExtensionProperties> extensions(extension_count);
@@ -37,6 +41,41 @@ namespace antartar
 			ranges::for_each(extensions, [](const VkExtensionProperties& prop) {
 				fmt::print("\t{}\n", prop.extensionName);
 				});
+		}
+
+		VkApplicationInfo app_info_() const
+		{
+			VkApplicationInfo app_info{};
+			app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+			app_info.pApplicationName = "antartar";
+			app_info.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
+			app_info.pEngineName = "no engine";
+			app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+			app_info.apiVersion = VK_API_VERSION_1_0;
+			return app_info;
+		}
+
+		VkInstanceCreateInfo create_info_() const
+		{
+			auto app_info = app_info_();
+			VkInstanceCreateInfo create_info{};
+			create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+			create_info.pApplicationInfo = std::addressof(app_info);
+			uint32_t glfw_extensions_count = 0;
+			create_info.ppEnabledExtensionNames = glfwGetRequiredInstanceExtensions(std::addressof(glfw_extensions_count));
+			create_info.enabledExtensionCount = glfw_extensions_count;
+			create_info.enabledLayerCount = 0;
+			return create_info;
+		}
+
+	public:
+		inline vk()
+		{
+			auto create_info = create_info_();
+			log_available_extensions();
+			if (enable_validation_layers && !check_validation_layer_support()) {
+				throw std::runtime_error(log_message("validation layers requested, but not available!"));
+			}
 
 
 			if (vkCreateInstance(std::addressof(create_info), nullptr, std::addressof(instance_)) != VK_SUCCESS)
@@ -45,19 +84,9 @@ namespace antartar
 			}
 		}
 
-		inline void cleanup_()
-		{
-			vkDestroyInstance(instance_, nullptr);
-		}
-
-	public:
-		inline vk()
-		{
-			initialize_();
-		}
 		inline ~vk()
 		{
-			cleanup_();
+			vkDestroyInstance(instance_, nullptr);
 		}
 	};
 }
