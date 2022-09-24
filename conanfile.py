77ec30a46b504c9342f216e9cb3627c2e939296e
maciejjablonsky <maciekjablonsky@gmail.com>
2022-09-24 13:33:26 +0200

    Fixed passing VK_LAYER_PATH and VK_INSTANCE_LAYERS

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout, CMake
-from conans.tools import environment_append
+from conan.tools.env import Environment, VirtualRunEnv
 
 class AntartarConanFile(ConanFile):
     name = "antartar"
@@ -17,33 +17,36 @@ class AntartarConanFile(ConanFile):
         deps = CMakeDeps(self)
         deps.generate()
 
+        # this creates custom script with custom env variables
+        vk_run_env = Environment()
+        if self.settings.build_type == "Debug":
+            vk_run_env.define("VK_INSTANCE_LAYERS", "VK_LAYER_LUNARG_api_dump;VK_LAYER_KHRONOS_validation")
+        vk_run_envvars = vk_run_env.vars(self, scope="run") # scopes the env variables to run environment
+        vk_run_envvars.save_script("vk_run_env") # saves a file
+
+        ms = VirtualRunEnv(self) 
+        ms.generate() # creates a launcher script which sets up dependencies runenv and the above custom run script
+
     def layout(self):
         cmake_layout(self)
     
     def build_requirements(self):
-        self.build_requires("shaderc/2021.1")
         self.build_requires("cmake/[>=3.24]")
+        self.build_requires("shaderc/2021.1")
 
     def requirements(self):
-        self.requires("vulkan-loader/1.3.216.0")
         self.requires("fmt/9.1.0")
         self.requires("glfw/3.3.8")
         self.requires("ms-gsl/4.0.0")
         self.requires("range-v3/0.12.0")
+        self.requires("vulkan-loader/1.3.216.0")
         self.requires("vulkan-validationlayers/1.3.216.0")
-
-    def _vulkan_layers(self):
-        layers = {}
-        if self.settings.build_type == "Debug":
-            layers["VK_INSTANCE_LAYERS"] = "VK_LAYER_LUNARG_api_dump;VK_LAYER_KHRONOS_validation"
-        return layers
 
 
     def build(self):
-        with environment_append(self._vulkan_layers()):
-            cmake = CMake(self)
-            if self.should_configure:
-                cmake.configure()
-            if self.should_build:
-                cmake.build()
-    
+        cmake = CMake(self)
+        if self.should_configure:
+            cmake.configure()
+        if self.should_build:
+            cmake.build()
+   
