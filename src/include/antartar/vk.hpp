156f93f149f1561f19a0aff5bef4f308d4432b98
maciejjablonsky <maciekjablonsky@gmail.com>
2022-12-03 09:27:22 +0100

    [tutorial] Added viewport definition

--- src/include/antartar/vk.hpp
+++ src/include/antartar/vk.hpp
@@ -642,6 +642,14 @@ class vk {
         input_assembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
         input_assembly.primitiveRestartEnable = VK_FALSE;
 
+        VkViewport viewport{};
+        viewport.x = 0.0f;
+        viewport.y = 0.0f;
+        viewport.width = static_cast<float>(swap_chain_extent_.width);
+        viewport.height = static_cast<float>(swap_chain_extent_.height);
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+
         vkDestroyShaderModule(device_, frag_shader_module, nullptr);
         vkDestroyShaderModule(device_, vert_shader_module, nullptr);
 
