60f46b550bae18ff10aa26b0f79939a4ecda1b59
maciejjablonsky <maciekjablonsky@gmail.com>
2023-02-17 17:17:15 +0100

    Added `tl::expected`

--- conanfile.py
+++ conanfile.py
@@ -129,6 +129,7 @@ class AntartarConanFile(ConanFile):
         if self.settings.build_type == "Debug":
             self.requires("vulkan-validationlayers/1.3.231.1")
         self.requires("shaderc/2021.1")
+        self.requires("tl-expected/20190710")
 
     def _build_shaders(self):
         # lame but enough way to deal with compilation
