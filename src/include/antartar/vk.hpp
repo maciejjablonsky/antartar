e761d972f508cdc0d6fee8b93c952ac9cc81bd92
maciejjablonsky <maciekjablonsky@gmail.com>
2023-03-04 12:00:24 +0100

    [Tutorial] Render pass

--- src/include/antartar/vk.hpp
+++ src/include/antartar/vk.hpp
@@ -94,6 +94,7 @@ class vk {
     VkFormat swap_chain_image_format_ = VkFormat::VK_FORMAT_UNDEFINED;
     VkExtent2D swap_chain_extent_{};
     std::pmr::vector<VkImageView> swap_chain_image_views_{};
+    VkRenderPass render_pass_;
     VkPipelineLayout pipeline_layout_;
 
     inline bool check_validation_layer_support_()
@@ -743,6 +744,43 @@ class vk {
         }
     }
 
+    auto create_render_pass_()
+    {
+        VkAttachmentDescription color_attachment{};
+        color_attachment.format         = swap_chain_image_format_;
+        color_attachment.samples        = VK_SAMPLE_COUNT_1_BIT;
+        color_attachment.loadOp         = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        color_attachment.storeOp        = VK_ATTACHMENT_STORE_OP_STORE;
+        color_attachment.stencilLoadOp  = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        color_attachment.initialLayout  = VK_IMAGE_LAYOUT_UNDEFINED;
+        color_attachment.finalLayout    = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        VkAttachmentReference color_attachment_ref{};
+        color_attachment_ref.attachment = 0;
+        color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+        VkSubpassDescription subpass{};
+        subpass.pipelineBindPoint    = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+        subpass.pColorAttachments    = std::addressof(color_attachment_ref);
+
+        VkRenderPassCreateInfo render_pass_info{};
+        render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+        render_pass_info.attachmentCount = 1;
+        render_pass_info.pAttachments    = std::addressof(color_attachment);
+        render_pass_info.subpassCount    = 1;
+        render_pass_info.pSubpasses      = std::addressof(subpass);
+
+        if (not equals(VK_SUCCESS,
+                       vkCreateRenderPass(device_,
+                                          std::addressof(render_pass_info),
+                                          nullptr,
+                                          std::addressof(render_pass_)))) {
+            throw std::runtime_error("failed to create render pass!");
+        }
+    }
+
   public:
     inline vk(auto& window)
     {
@@ -753,12 +791,14 @@ class vk {
         create_logical_device_();
         create_swap_chain_(window);
         create_image_views_();
+        create_render_pass_();
         create_graphics_pipeline_();
     }
 
     inline ~vk()
     {
         vkDestroyPipelineLayout(device_, pipeline_layout_, nullptr);    
+        vkDestroyRenderPass(device_, render_pass_, nullptr);
         ranges::for_each(swap_chain_image_views_,
                          [this](const VkImageView& image_view) {
                              vkDestroyImageView(device_, image_view, nullptr);
