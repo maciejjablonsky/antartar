a4a84e521334b4f0f91890fb419ce8f1e92b2bcc
maciejjablonsky <maciekjablonsky@gmail.com>
2023-03-19 19:17:26 +0100

    struct named member initialization

--- src/include/antartar/vk.hpp
+++ src/include/antartar/vk.hpp
@@ -342,29 +342,28 @@ class vk {
         auto queue_priority                      = 1.0f;
 
         for (uint32_t queue_family : unique_queue_families) {
-            VkDeviceQueueCreateInfo queue_create_info{};
-            queue_create_info.sType =
-                VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-            queue_create_info.queueFamilyIndex = queue_family;
-            queue_create_info.queueCount       = 1;
-            queue_create_info.pQueuePriorities = std::addressof(queue_priority);
+            VkDeviceQueueCreateInfo queue_create_info{
+                .sType            = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+                .queueFamilyIndex = queue_family,
+                .queueCount       = 1,
+                .pQueuePriorities = std::addressof(queue_priority),
+            };
             queue_create_infos.push_back(queue_create_info);
         }
 
         VkPhysicalDeviceFeatures device_features{};
 
-        VkDeviceCreateInfo create_info{};
-        create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        VkDeviceCreateInfo create_info{
+            .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+            .queueCreateInfoCount =
+                static_cast<uint32_t>(queue_create_infos.size()),
+            .pQueueCreateInfos = queue_create_infos.data(),
+            .enabledExtensionCount =
+                static_cast<uint32_t>(device_extensions.size()),
+            .pEnabledFeatures  = std::addressof(device_features),
+            .ppEnabledExtensionNames = device_extensions.data()
 
-        create_info.queueCreateInfoCount =
-            static_cast<uint32_t>(queue_create_infos.size());
-        create_info.pQueueCreateInfos = queue_create_infos.data();
-
-        create_info.pEnabledFeatures = std::addressof(device_features);
-        create_info.enabledExtensionCount =
-            static_cast<uint32_t>(device_extensions.size());
-        create_info.ppEnabledExtensionNames = device_extensions.data();
-
+        };
         if (enable_validation_layers) {
             create_info.enabledLayerCount =
                 static_cast<uint32_t>(validation_layers.size());
